76b83c4c9a93ae87aef8604fc80d3465883a17e9
fevio <mailto:13570021+Fevio@users.noreply.github.com>
2022-03-11 17:38:10 +0100

    first commit

---
+++ src/test/java/prj/fl/roulettesimulator/AppTest.java
@@ -0,0 +1,20 @@
+package prj.fl.roulettesimulator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
